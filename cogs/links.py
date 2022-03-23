93fe60496e11c02d164a1f42dda616e2ca071864
Samuel Burkhardt <samuel.burkhardt.418@gmail.com>
2022-03-23 17:05:02 +0100

    Initial Commit

---
+++ cogs/links.py
@@ -0,0 +1,81 @@
+from discord.ext import commands
+
+
+class LinkCommands(commands.Cog, name="Links"):
+    """Commands linking to external websites or past discussions"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["mif", "format"])
+    async def wiki(self, ctx):
+        """Minecraft Map Item Format on the Minecraft Wiki"""
+        message = "https://minecraft.gamepedia.com/Map_item_format"
+
+        await ctx.send(message)
+
+    @commands.command(aliases=["mac"])
+    async def mapartcraft(self, ctx):
+        """Map Art Generator made by rebane2001#3716"""
+        message = (
+            "https://rebane2001.com/mapartcraft/\n"
+            "made with :heart: by rebane2001#3716"
+        )
+
+        await ctx.send(message)
+
+    @commands.command(aliases=["maw", "wall"])
+    async def mapartwall(self, ctx):
+        """Map Art Wall made by rebane2001#3716"""
+        message = (
+            "https://rebane2001.com/mapartwall/\n"
+            "made with :heart: by rebane2001#3716"
+        )
+
+        await ctx.send(message)
+
+    @commands.command()
+    async def bookart(self, ctx):
+        """Like Map Art but in books"""
+        message = (
+            "Check out the discussion here:\n"
+            "https://discord.com/channels/349201680023289867/368137692099379209/930486853918933032 "
+            "(scroll down for more screenshots)"
+        )
+
+        await ctx.send(message)
+
+    @commands.command()
+    async def moire(self, ctx):
+        """It's like acid"""
+        message = (
+            "Check out the discussion here:\n"
+            "https://discord.com/channels/349201680023289867/349480851915931653/919933674177306646"
+        )
+
+        await ctx.send(message)
+
+    @commands.command(aliases=["prio", "queue", "prioq", "prioqueue"])
+    async def priorityqueue(self, ctx):
+        """The official 2b2t shop"""
+        message = "https://shop.2b2t.org (don't do it though)"
+
+        await ctx.send(message)
+
+    @commands.command()
+    async def invite(self, ctx):
+        """Discord invite for this server"""
+        message = "https://discord.gg/r7Tuerq"
+
+        await ctx.send(message)
+
+    @commands.command()
+    async def baritone(self, ctx):
+        """Link to the baritone github repo"""
+        message = "https://github.com/cabaletta/baritone"
+
+        await ctx.send(message)
+
+
+def setup(client):
+    client.add_cog(LinkCommands(client))
---
+++ main.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import config
+
+
+bot = commands.Bot(command_prefix=config.prefix, case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('Guilds:')
+    print("\n".join("* " + g.name for g in (await bot.fetch_guilds().flatten())))
+
+    cogs = ["cogs.memes", "cogs.help", "cogs.links", "cogs.misc", "cogs.exceptions"]
+
+    for cog in cogs:
+        bot.load_extension(cog)
+
+    print('------')
+
+
+@bot.check
+async def ignore_dms(ctx):
+    return ctx.guild is not None
+
+
+bot.run(config.token)
