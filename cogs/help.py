8abdb42e06eea3e268c55e504a8863a8535e9bdc
Samuel Burkhardt <samuel.burkhardt.418@gmail.com>
2024-01-29 21:05:32 +0100

    add more description

--- cogs/help.py
+++ cogs/help.py
@@ -122,7 +122,9 @@ class HelpCommands(commands.Cog, name="Help"):
             "maps with baritone.\n"
             "> The setting `#mapArtMode true` makes it only care about the top block in each column, which is useful "
             "for staircased maps. On the contrary, the *buildInLayers* setting should be disabled.\n"
-            "Baritone: https://github.com/cabaletta/baritone\n"
+            "Baritone: <https://github.com/cabaletta/baritone>\n"
+            "If you need to convert your .NBT file output from Mapartcraft into a .LITEMATIC, follow the guide bellow. "
+            "Baritone only reads .litematic files.\n"
             "https://media.discordapp.net/attachments/565597105629036557/1201107698574438500/Capture_decran_2024-01-28_103628.png"
         )
 
