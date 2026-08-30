c5f04959c5980f0d1cd65e314c09d45965c2eb19
Aryezz <43098166+aryezz@users.noreply.github.com>
2026-08-30 10:43:43 +0200

    update spam detection with punitive action
    detected spammers now get an info message DMed, get kicked and the spam messages are deleted

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,12 +1,15 @@
 import dataclasses
-from datetime import datetime, timezone, timedelta
 import hashlib
+import logging
 from collections import defaultdict
+from datetime import datetime, timezone, timedelta
 
 import discord
 from discord.ext import commands, tasks
 
 import config
+
+_logger = logging.getLogger("discord.moderation")
 
 
 def _normalize(text: str) -> str:
@@ -18,9 +21,30 @@ def _hash_message(message: discord.Message) -> str:
     return hashlib.sha256(_normalize(hash_string).encode("utf-8")).hexdigest()
 
 
+_kick_message = (
+        "You have been kicked from Map Artists of 2b2t <:mcmap:349454913526562816> for suspected spam.\n\n" +
+        "If you were not posting there yourself moments ago, your Discord account has likely been compromised and is "
+        "being actively used by spammers as you read this. You should immediately change your password to lock them out "
+        "(seriously, stop reading and do that right now), and try to find and block through what means your information "
+        "was stolen. Otherwise they may still be able to get into your account again despite the password change.\n"
+        "The most common ways people get compromised are fake login forms on phishing sites, malware, or bots you "
+        "unknowingly authorized to post on your behalf. Phishing sites usually can't get back in after a password "
+        "change, and you can deauthorize some bots in Discord's settings. Identifying malware is beyond the scope of "
+        "this message, as it can be difficult to track down and is not always detected by antivirus. However, if this "
+        "happens again after a password change and deauthorizing bots, you know it was caused by something you used, or "
+        "that ran, in the time since.\n\n"
+        "When you have properly secured your account, you can rejoin."
+)
+
+
+def _boolean_emoji(boolean: bool):
+    return ":white_check_mark:" if boolean else ":x:"
+
+
 @dataclasses.dataclass
 class MessageRecord:
     first_seen: datetime
+    author: discord.Member
     messages: list[discord.Message]
 
     def get_channel_ids(self) -> set[int]:
@@ -29,6 +53,7 @@ class MessageRecord:
 
 class Moderation(commands.Cog, name="Moderation"):
     """Server moderation tools"""
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         # cache[user_id][hash]: MessageRecord
@@ -46,12 +71,47 @@ class Moderation(commands.Cog, name="Moderation"):
         now = datetime.now(timezone.utc)
         cutoff = now - timedelta(seconds=30)
         for user_id, records in list(self.cache.items()):
-            filtered_records = {message_hash: record for message_hash, record in records.items() if record.first_seen >= cutoff}
+            filtered_records = {message_hash: record for message_hash, record in records.items() if
+                                record.first_seen >= cutoff}
 
             if not filtered_records:
                 del self.cache[user_id]
             else:
                 self.cache[user_id] = filtered_records
+
+    @staticmethod
+    async def _remove_spam(record: MessageRecord) -> tuple[bool, bool, int]:
+        sent_kick_message = False
+        try:
+            await record.author.send(_kick_message)
+            sent_kick_message = True
+        except discord.Forbidden:
+            _logger.error("No permission to send kick message")
+        except discord.HTTPException:
+            _logger.error("HTTP exception while sending kick message")
+
+        kicked_member = False
+        try:
+            await record.author.kick(reason=f"spam detected")
+            kicked_member = True
+        except discord.Forbidden:
+            _logger.error("No permission to kick member")
+        except discord.HTTPException:
+            _logger.error("HTTP exception while kicking member")
+
+        deleted_count = 0
+        for message in record.messages:
+            try:
+                await message.delete()
+                deleted_count += 1
+            except discord.Forbidden:
+                _logger.error("No permission to delete message")
+            except discord.NotFound:
+                _logger.error("Message to delete could not be found")
+            except discord.HTTPException:
+                _logger.error("HTTP exception while deleting message")
+
+        return sent_kick_message, kicked_member, deleted_count
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -73,12 +133,15 @@ class Moderation(commands.Cog, name="Moderation"):
             record.messages.append(message)
             message_count = len(record.get_channel_ids())
             if record.first_seen >= cutoff and message_count >= 3:
-                # temporary action: don't kick user or delete any messages
-                # TODO: add kick and message deletion when we're sure that there are no false positives
-                message = f"detected spammer {message.author.mention} in {message_count} channels"
+                msg_sent, kicked, delete_count = await self._remove_spam(record)
+
+                message = (f"detected spammer {message.author.mention}\n" +
+                           f"kick message sent: {_boolean_emoji(msg_sent)}\n" +
+                           f"member kicked: {_boolean_emoji(kicked)}\n" +
+                           f"messages deleted: {delete_count}/{len(record.messages)}")
                 await self.bot_log_channel.send(message)
         else:
-            self.cache[message.author.id][message_hash] = MessageRecord(message.created_at, [message])
+            self.cache[message.author.id][message_hash] = MessageRecord(message.created_at, message.author, [message])
 
 
 async def setup(client):
